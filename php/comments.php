ec71b29de601fc4becafe6f04aa06ef280edef10
Neydorfin <iongal.02@gmail.com>
2022-04-20 17:27:52 +0300

    changed

---
+++ php/comments.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>EU</title>
+        <link href="../css/style.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="../images/Eur.png" type="image/x-icon">
+
+    </head>
+    <body>
+    <button id="upBtn" onclick="scrollBack()"><img src="/images/buttonUp.png" alt="buttonUp"></button>
+    <header>
+        <a href="../index.html" class="header_logo_text" >
+            <img src="../images/EuropeanS.png" alt="European_union_flag" class="header_logo_image">
+            European Union
+        </a>
+        <nav >
+            <a href="../html/map.html" class="nav_link">Map</a>
+            <a href="../html/country.html" class="nav_link">Country</a>
+            <a href="../html/culture.html" class="nav_link">Culture</a>
+        </nav>
+    </header>
+    <div class="back_canvas">
+
+    </div>
+    <footer class="footer">
+        <nav class="nav_footer">
+            <a class="a_footer" href="../php/contacts.php">Contacts</a>
+            <a class="a_footer" href="../php/subscribe.php">Subscribe</a>
+            <a class="a_footer" href="../php/comments.php">Comments</a>
+        </nav>
+        <p class="p_footer">&#169 Galinschii Ion IA-211</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21818128.107613463!2d4.113195771301079!3d48.12285598107331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ed8886cfadda85%3A0x72ef99e6b3fcf079!2z0JXQstGA0L7Qv9Cw!5e0!3m2!1sru!2s!4v1650434562089!5m2!1sru!2s" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </footer>
+    <script src="/scripts/scrollBack.js"></script>
+    </body>
+</html>
+
+
